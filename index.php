bc29418888496a74ea2b23c0a2265117ef7d5ef6
Denace <kiemeden@gmail.com>
2019-07-18 11:40:17 +0300

    prettify page

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 <head>
     <title>My Feedback</title>
     <style>
-        body {
-            width: 550px;
-            font-family: Arial;
-        }
-
         #frm-qr {
             padding: 20px 40px;
             background: #CCC;
@@ -18,6 +13,7 @@
             border: 0px;
             border-radius: 3px;
             width: 250px;
+            align: right;
         }
 
         .submit-button {
@@ -81,20 +77,83 @@
      <script src="/Myfeedback/bootstrap.min.js"></script>
 </head>
 <body>
-    <form method="post" name="feedback" id="frm-qr"action="index.php">
-        <div class="form-row">
-            Name: <input type="text" name="name_field" id="name_field" class="input-field" />
+<div class="container">
+    <nav class="navbar sticky-top navbar-light bg-light">
+        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="#frm">Feedback</a>
+        <a class="nav-link" href="#address">Contacts</a>
+    </nav>
+    <div class="row justify-content-center" > 
+    <div class="col-md-auto">  
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block rounded" src="images/ny.jpg" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>New York</h3>
+                            <p>this is a NY concert</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block rounded" src="images/chicago.jpg" alt="Second slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>Chicago</h3>
+                            <p>A concert in Illinois</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block rounded" src="images/la.jpg" alt="Third slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h3>Los Angeles</h3>
+                            <p>LA is humid and fun!</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
         </div>
-        <div class="form-row">
-            Email: <input type="email" name="email_field" id="email_field" class="input-field" />
-        </div>
-        <div class="form-row">
-            Comments: <textarea rows="10" cols="10" class="input-field" name="comments_field" id="comments_field" ></textarea>
-        </div>
-        <div>
-            <input type="submit" name="submit" class="submit-button"value="Submit" />           
-        </div>
-    </form>  
+    </div>
+
+    <div class="row justify-content-center"> 
+        <div class="col-md-5">
+            <h2 class="display-3" id="frm">Take a few minutes to give feedback!</h2> <br/>
+            <form method="post" name="feedback" id="frm-qr"action="index.php">
+                <div class="form-row">
+                    Name: <input type="text" name="name_field" id="name_field" class="input-field"  required/>
+                </div>
+                <div class="form-row">
+                    Email: <input type="email" name="email_field" id="email_field" class="input-field" required />
+                </div>
+                <div class="form-row">
+                    Comments: <textarea rows="10" cols="10" class="input-field" name="comments_field" id="comments_field" required></textarea>
+                </div>
+                <div>
+                    <input type="submit" name="submit" class="submit-button"value="Submit" />           
+                </div>
+            </form>
+        </div> 
+    </div> 
+    <div class="row justify-content-center"> 
+        <address id="address">
+                <b>Ivy Gitiba</b><br/>
+                <Label>Phone Number:</label> +254 658 254<br/>
+                <Label>IG:</label>myig<br/>
+                <Label>Address:</label> 21346 Links Road<br/>
+        </address>
+    </div>
+</div>  
 </body>
 
 <?php
@@ -107,8 +166,7 @@
 
     if(isset($email,$comments)){
         save_comment($email,$comments);
-        //get_comment();
-        //echo $email." ". $name." ". $comments ;
+        get_comments();
     }
 
     function save_comment($k,$c){
@@ -130,7 +188,7 @@
         }
         
     }
-    function get_comment(){
+    function get_comments(){
         $conn=OpenCon();
         $sql="select * from comment";
         $result=$conn->query($sql);
@@ -139,6 +197,9 @@
         }
 
     }
+    /**
+     * To be implemented edit and delete functions
+     */
     function delete_comment($id){}
     function edit_comment($id){}
 
